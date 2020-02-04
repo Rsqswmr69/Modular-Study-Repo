f90445276ec11e53f85caeb03ffacc995925ca9a
caleward <caleward9@gmail.com>
2020-02-04 14:27:28 +0430

    Add files via upload

--- Questions.java
+++ Questions.java
@@ -15,16 +15,36 @@ import javax.swing.JRadioButton;
 import javax.swing.JProgressBar;
 import javax.swing.JButton;
 import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
+
 import java.awt.event.ActionListener;
+import java.util.Collections;
+import java.util.List;
 import java.awt.event.ActionEvent;
 
 public class Questions extends JFrame {
 	
+	/*
+	 * List to hold the Questions and Answers
+	 */
+	private List<Question> questionList;
+	private int questionCounter;
+	private int questionCountTotal;
+	private Question currentQuestion;
+	
 	public int totalQuestions;
 	public int questionsLeft;
 	public double score;
-	boolean flag=false;
+	boolean answered;
 	
+	
+	
+	
+	JRadioButton bRadioButton = new JRadioButton("b)");
+	JRadioButton cRadioButton = new JRadioButton("c)");
+	JRadioButton dRadioButton = new JRadioButton("d)");
+	JRadioButton aRadioButton = new JRadioButton("a)");	
+	JLabel questionLabel = new JLabel("");
 	private JPanel contentPane;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 
@@ -44,6 +64,7 @@ public class Questions extends JFrame {
 		});
 	}
 
+	
 	/**
 	 * Create the frame.
 	 */
@@ -55,31 +76,22 @@ public class Questions extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		/*
-		 * Database Parser needs to input first question here and subsequent
-		 * questions each time a new question is needed
-		 */
-		JLabel questionLabel = new JLabel("Questions Goes Here\n");
+			
 		questionLabel.setBounds(148, 12, 477, 88);
 		contentPane.add(questionLabel);
 		
-		JRadioButton aRadioButton = new JRadioButton("a)");
 		buttonGroup.add(aRadioButton);
 		aRadioButton.setBounds(148, 150, 149, 23);
 		contentPane.add(aRadioButton);
 		
-		JRadioButton bRadioButton = new JRadioButton("b)");
 		buttonGroup.add(bRadioButton);
 		bRadioButton.setBounds(148, 195, 149, 23);
 		contentPane.add(bRadioButton);
 		
-		JRadioButton cRadioButton = new JRadioButton("c)");
 		buttonGroup.add(cRadioButton);
 		cRadioButton.setBounds(148, 240, 149, 23);
 		contentPane.add(cRadioButton);
 		
-		JRadioButton dRadioButton = new JRadioButton("d)");
 		buttonGroup.add(dRadioButton);
 		dRadioButton.setBounds(148, 285, 149, 23);
 		contentPane.add(dRadioButton);
@@ -90,63 +102,72 @@ public class Questions extends JFrame {
 		
 		JButton submitButton = new JButton("Submit");
 		submitButton.setToolTipText("Click submit to answer the question\n");
+		
+		dbHelper dbHelper = new dbHelper();
+		questionList = dbHelper.getQuestions();
+		questionCountTotal = questionList.size();
+		Collections.shuffle(questionList);
+		
+		showQuestion();
+		
 		submitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				/*
 				 * Check to see if a button is selected
 				 */
-				if (buttonGroup.getSelection()==null)
-					flag = false;
-				else 
-					flag = true;
-				
-				
-				/*
-				 * Create the Results window when all questions have been answered, add one to the score
-				 * 
-				 */
-				if(questionsLeft==0) {
-					/*
-					 * if no button is selected return
-					 */
-					if (flag==false) {
+				if(!answered) {
+					if (aRadioButton.isSelected()||bRadioButton.isSelected()||cRadioButton.isSelected()||dRadioButton.isSelected()) {
+						checkAnswer();
+					}
+					else {
 						JOptionPane.showMessageDialog(submitButton, "Why not guess an answer if you do not know it?");
-						return;
 					}
-					/*
-					 * if the right answer is selected add 1 to the score
-					 */
-					if(bRadioButton.isSelected()) {
-						score++;
-					}
-					dispose(); 
-					Results result = new Results();
-					result.setVisible(true);
 				}
-				
 				else {
-					if (flag=false) {
-						JOptionPane.showMessageDialog(submitButton, "Why not guess an answer if you do not know it?");
-					
-					}
-					/*
-					 * Checks whether the answer is correct and restarts the window with a new question
-					 * 
-					 */
-					if(bRadioButton.isSelected()) {
-						score++;
-					}
-					questionsLeft--;
-				//	progressBar.update();
-					dispose();
-					Questions q = new Questions();
-					q.setVisible(true);
-					
+					showQuestion();
 				}
+					
 			}
+			
 		});
 		submitButton.setBounds(509, 355, 182, 43);
 		contentPane.add(submitButton);
 		
 	}
+	private void checkAnswer() {
+		answered = true;
+		
+		/*
+		 * Need to check the answer here
+		 */
+		
+	//	ButtonModel rbSelected = buttonGroup.getSelection();
+	//	int answerNr = buttonGroup.indexOfChild(rbSelected);
+//		if(answerNr == currentQuestion.getAnswerNr()) {
+	//		score++;
+		}
+		
+		 
+	
+	private void showQuestion() {
+		buttonGroup.clearSelection();
+		answered=false;
+		if(questionCounter < questionCountTotal) {
+			currentQuestion = questionList.get(questionCounter);
+			questionLabel.setText(currentQuestion.getQuestion());
+			aRadioButton.setText(currentQuestion.getOption1());
+			bRadioButton.setText(currentQuestion.getOption2());
+			cRadioButton.setText(currentQuestion.getOption3());
+			dRadioButton.setText(currentQuestion.getOption4());
+			questionCounter++;
+			
+		}
+		else {
+			Results results= new Results();
+			results.setVisible(true);
+		}
+	}
 }
+      	
+     
+	
