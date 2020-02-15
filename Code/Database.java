fea887c598dd5f66baf319dcdd386687b7b7e7ca
dtsawyer <60196408+dtsawyer@users.noreply.github.com>
2020-02-14 20:22:49 -0500

    Add files via upload

---
+++ Code/Database.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// Run and compile with:
+// javac -cp ".:./mysql-connector-java-8.0.19.jar" Main.java 
+// java -cp ".:./mysql-connector-java-8.0.19.jar" Main
+// On windows replace : with ;
+
+// Main class will be initiliazed by user
+public class Database {
+
+    // Set static variables for database connection
+    private static Connection conn = null;
+    private static Statement stmt = null;
+    private static ResultSet results = null;
+    private static String USER = "nEHRVrOue2";
+    private static String PASSWORD = "PH7592K95R";
+    private static String DATABASE = "nEHRVrOue2";
+    private static String DOMAIN = "remotemysql.com";
+    private static String URL = "jdbc:mysql://" + DOMAIN + "/" + DATABASE;
+    private static ArrayList<String> categories = new ArrayList<String>();
+    private static ArrayList<HashMap<String, String>> questions = new ArrayList<HashMap<String, String>>();
+
+    // Default database constructor
+    public Database() throws Exception {
+        try {
+            // Setup initial database connection
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            String q = String.format("SELECT * from categories_tbl");
+            results = conn.createStatement().executeQuery(q);
+
+            // Extract categories and save them to class variable
+            while (results.next()) {
+                String category = results.getString("category");
+                categories.add(category);
+            }
+        } catch (SQLException e) {e.printStackTrace();}
+    }
+
+    // Returns all categories from database
+    public static ArrayList<String> getCategories() {return categories;}
+    
+    // Returns a single question for a given question ID
+    public static ArrayList<HashMap<String, String>> getQuestion(int questionID) {
+
+        ArrayList<HashMap<String, String>> question = new ArrayList<HashMap<String, String>>();
+
+        for (HashMap<String, String> q: questions)
+        {
+            if (Integer.parseInt(q.get("id")) == questionID) {question.add(q);}
+        }
+        return question;
+    }
+
+    // Set category and initialize all questions from that category
+    public static void setCategory(String category) throws Exception  {
+        try {
+            String q = String.format("SELECT questions_tbl.question_id, questions_tbl.question_text, categories_tbl.category, choices_tbl.choice_text, choices_tbl.is_correct FROM questions_tbl INNER JOIN categories_tbl ON questions_tbl.question_category = categories_tbl.category_id INNER JOIN choices_tbl on questions_tbl.question_id = choices_tbl.question_id WHERE category = '%s'", category);
+            results = conn.createStatement().executeQuery(q);
+
+            // Collect all questions from category and store them in an ArrayList of questions
+            while (results.next()) {
+                HashMap<String, String> question = new HashMap<>();
+                question.put("id", results.getString("question_id"));
+                question.put("question", results.getString("question_text"));
+                question.put("choice", results.getString("choice_text"));
+                question.put("value", results.getString("is_correct"));
+                questions.add(question);
+            }
+        } catch (SQLException e) {e.printStackTrace();}
+    }
+}
+
--- Code/Main.java
+++ Code/Main.java
@@ -1,10 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 
 // Run and compile with:
 // javac -cp ".:./mysql-connector-java-8.0.19.jar" Main.java 
@@ -12,7 +6,7 @@ import java.util.List;
 // On windows replace : with ;
 
 // TODO
-// Parse stuff from SQL into a test format (see queries.txt)
+// Randomize questions pulled from database
 // Handle individul GUI components from Main.java, track scores
 
 // Main class will be initiliazed by user
@@ -20,59 +14,18 @@ public class Main {
 
     // Method calls and sequencing will go here
     public static void main(String[] args) throws Exception {
-        
-        InitialWindow gui = new InitialWindow();
-        gui.setVisible(true);
-        getQuestions("Geography");
-    }
-
-
-
-    // Testing SQL connection and dumping contents
-    public static void getQuestions(String category) throws Exception {
-
-        // write your code here
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet results = null;
-
-        String USER = "nEHRVrOue2";
-        String PASSWORD = "PH7592K95R";
-        String DATABASE = "nEHRVrOue2";
-        String DOMAIN = "remotemysql.com";
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            System.out.println("Connecting");
-            conn = DriverManager.getConnection(
-                    "jdbc:mysql://" + DOMAIN + "/" + DATABASE, USER, PASSWORD);
-            System.out.println("Connected");
-
-            // create query
-            stmt = conn.createStatement();
-            String sql = String.format("SELECT questions_tbl.question_id, questions_tbl.question_text, categories_tbl.category FROM questions_tbl INNER JOIN categories_tbl ON questions_tbl.question_category = categories_tbl.category_id WHERE category = '%s'", category);
-
-            // execute query store results into results
-            results = stmt.executeQuery(sql);
-
-            // loop through results
-            
-            while (results.next()) {
-                // store variable based on column name
-                int questionId = results.getInt("question_id");
-                String questionText = results.getString("question_text");
-
-                // prints all questions
-                System.out.println(questionText);
-            }
-            
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        Database db = new Database();
+        db.setCategory("Geography");
+        for(int i=0; i <= 10; i++) {
+            System.out.println("Question " + i);
+            System.out.println(db.getQuestion(i));
+            System.out.println();
         }
-       
+        //InitialWindow gui = new InitialWindow();
+        //gui.setVisible(true);
+        //getQuestions("Geography");
     }
-    
+
     /*
      * Method that puts the question and answer into a
      * GUI friendly format. (Cale, 12 FEB)
@@ -90,5 +43,6 @@ public class Main {
 		questionList.add(question);
 		return questionList;
     }
-        
+
 }
+
