77712cd0b1ae0f100ca7ec7c9e1bf7ff83cbe6c4
caleward <caleward9@gmail.com>
2020-02-20 11:02:19 +0430

    Update Main.java

--- Code/Main.java
+++ Code/Main.java
@@ -12,37 +12,23 @@ import java.util.List;
 // Main class will be initiliazed by user
 public class Main {
 
+	
+	
     // Method calls and sequencing will go here
     public static void main(String[] args) throws Exception {
-        Database db = new Database();
-        db.setCategory("Geography");
-        for(int i=0; i <= 10; i++) {
-            System.out.println("Question " + i);
-            System.out.println(db.getQuestion(i));
-            System.out.println();
-        }
+   //     Database db = new Database();
+   //     db.setCategory("Geography");
+   //    for(int i=0; i <= 10; i++) {
+   //         System.out.println("Question " + i);
+   //         System.out.println(db.getQuestion(i));
+   //        System.out.println();
+   //     }
         //InitialWindow gui = new InitialWindow();
         //gui.setVisible(true);
-        //getQuestions("Geography");
+	//getQuestions("Geography");
+	    InitialWindow iW = new InitialWindow();
+	    iW.setVisible(true);
     }
-
-    /*
-     * Method that puts the question and answer into a
-     * GUI friendly format. (Cale, 12 FEB)
-     */
-    public List<Question> getQuestions() {
-		List<Question> questionList = new ArrayList<>();
-		//access database
-		Question question = new Question();
-		question.setQuestion("add question from db");  
-		question.setOption1("add opt 1 from db");
-		question.setOption2("add opt 2 from db");
-		question.setOption3("add opt 3 from db");
-		question.setOption4("add opt 4 from db");
-		question.setAnswerNr(2); //from db
-		questionList.add(question);
-		return questionList;
-    }
-
+        
 }
 
