5f37e518d6298024da050a9fff42918eb7757bee
caleward <caleward9@gmail.com>
2020-02-20 10:54:26 +0430

    Add files via upload

--- Code/InitialWindow.java
+++ Code/InitialWindow.java
@@ -17,6 +17,8 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.event.ActionEvent;
 
 public class InitialWindow extends JFrame {
@@ -46,8 +48,9 @@ public class InitialWindow extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * @throws Exception 
 	 */
-	public InitialWindow() {
+	public InitialWindow() throws Exception {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 739, 457);
@@ -62,10 +65,10 @@ public class InitialWindow extends JFrame {
 		 */
 		JComboBox subjectComboBox = new JComboBox();
 		subjectComboBox.setToolTipText("Click the arrow and select the course you would like to study");
-		subjectComboBox.setModel(new DefaultComboBoxModel(new String[] {"Subject A", "Subject B", "Subject C", "Subject D"}));
+		subjectComboBox.setModel(new DefaultComboBoxModel(new String[] {"Geography", "Math", "Subject C", "Subject D"}));
 		subjectComboBox.setBounds(109, 50, 246, 24);
 		contentPane.add(subjectComboBox);
-		
+				
 		JLabel numberOfQuestionsAvailable = new JLabel("# of Questions Available");
 		numberOfQuestionsAvailable.setToolTipText("The number of Questions available to study in this subject");
 		numberOfQuestionsAvailable.setFont(new Font("Monospaced", Font.BOLD, 15));
@@ -77,7 +80,25 @@ public class InitialWindow extends JFrame {
 		 * The parser should find the number of questions available and display it where I 
 		 * have hardcoded a 9 in. 
 		 */
-		JLabel actualNumber = new JLabel("9");
+		
+		JLabel actualNumber = new JLabel();
+
+		if(subjectComboBox.getSelectedItem().equals("Geography")) {
+			int questionCountTotal;
+			List <Question> questionList = new ArrayList<>();
+			Database db = new Database();
+			questionList = db.createQuestionList("Geography");
+			questionCountTotal = questionList.size();
+			actualNumber.setText(Integer.toString(questionCountTotal));
+		}
+		if(subjectComboBox.getSelectedItem().equals("Math")) {
+			int questionCountTotal;
+			List <Question> questionList = new ArrayList<>();
+			Database db = new Database();
+			questionList = db.createQuestionList("Math");
+			questionCountTotal = questionList.size();
+			actualNumber.setText(Integer.toString(questionCountTotal));
+		}
 		actualNumber.setToolTipText("The number of Questions available to study in this subject");
 		actualNumber.setBounds(431, 135, 206, 24);
 		contentPane.add(actualNumber);
@@ -119,9 +140,16 @@ public class InitialWindow extends JFrame {
 				 * Closes the window and opens up the question window
 				 */
 				else {
-				dispose();
-				Questions q = new Questions();
-				q.setVisible(true);
+					dispose();
+					Questions questions;
+					try {
+						questions = new Questions();
+						questions.setVisible(true);
+
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 				}
 			}
 		});
