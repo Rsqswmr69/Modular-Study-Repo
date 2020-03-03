59adaa9929f71bcf9e83c39a83616709e42c3a9e
dtsawyer <60196408+dtsawyer@users.noreply.github.com>
2020-03-03 15:11:16 -0800

    Add files via upload

--- Code/Choice.java
+++ Code/Choice.java
@@ -1,36 +1,20 @@
+// Helper class for storing answers for Question class
 public class Choice {
     private int choiceId;
     private String choiceText;
     private int isCorrect;
 
-    public Choice() {}
+	// Getters
+    public int getChoiceId() {return choiceId;}
+    public String getChoiceText() {return choiceText;}
+    
+	// Setters
+    public void setChoiceId(int choiceId) {this.choiceId = choiceId;}
+    public void setChoiceText(String choiceText) {this.choiceText = choiceText;}
+    public void setIsCorrect(int isCorrect) {this.isCorrect = isCorrect;}
 
-    public int getChoiceId() {
-        return choiceId;
-    }
-
-    public String getChoiceText() {
-        return choiceText;
-    }
-
-    public int isCorrect() {
-        return isCorrect;
-    }
-
-    public void setChoiceId(int choiceId) {
-        this.choiceId = choiceId;
-    }
-
-    public void setChoiceText(String choiceText) {
-        this.choiceText = choiceText;
-    }
-
-    public void setIsCorrect(int isCorrect) {
-        this.isCorrect = isCorrect;
-    }
-
-    @Override
-    public String toString() {
-        return choiceText + " " + isCorrect;
-    }
+	// Misc
+	@Override
+    public String toString() {return choiceText + " " + isCorrect;}
+	public int isCorrect() {return isCorrect;}
 }
